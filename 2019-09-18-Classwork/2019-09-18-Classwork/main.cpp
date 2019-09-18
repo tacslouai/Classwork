6cffbaefe33342f50fb713b94d0e8e05989abaca
tacslouai <55443256+tacslouai@users.noreply.github.com>
2019-09-18 08:22:40 -0400

    Add files via upload

---
+++ 2019-09-18-Classwork/2019-09-18-Classwork/main.cpp
@@ -0,0 +1,12 @@
+// Name: Louai Hammad
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// Classwork 2019-09-18
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "What's poppin' my dude" << std::endl;
+}
