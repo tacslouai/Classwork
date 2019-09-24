bb487fc5432b9f7ce348e8e07060ad2c041a48a5
tacslouai <55443256+tacslouai@users.noreply.github.com>
2019-09-24 08:40:38 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,19 @@
+// Name: Louai Hammad
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-09-24-Classwork
+
+#include<iostream>
+
+int main()
+{
+	int dab = 42;
+	char lol = 124;
+	bool naenae = 31;
+	double x = 1129;
+	float d = 523;
+
+		std::cout << sizeof(dab + lol + naenae + x + d) << std::endl;
+}
+
