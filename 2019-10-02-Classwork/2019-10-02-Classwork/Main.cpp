73a42d058c7bbe101cab3970ebed3f5421b9b374
tacslouai <55443256+tacslouai@users.noreply.github.com>
2019-10-02 08:38:13 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/Main.cpp
@@ -0,0 +1,26 @@
+// Name: Louai Hammad
+// Intro to CS
+// Period 1
+// Mr. Williams
+// Classwork 2019-10-02
+
+#include <iostream>
+
+int main()
+{
+	char letterX = 'X';
+	char letterB = 'B';
+	char letterABC = '496';
+	char letterY = 'sab';
+
+	int dabStyle = 410;
+	short madLad = 3105;
+	long absoluteUnit = 1410;
+
+	bool dab = true;
+	bool shab = false;
+
+	float sokoLibaTooka = 12441214;
+	float wasdQwerty = 6135137;
+	double loonyToons = 12412412412124;
+}
