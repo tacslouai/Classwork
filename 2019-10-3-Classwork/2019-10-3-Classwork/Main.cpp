091da8f4d74a159bbabbefc7522d0c3ceb4e1a64
tacslouai <55443256+tacslouai@users.noreply.github.com>
2019-10-03 08:38:59 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/Main.cpp
@@ -0,0 +1,19 @@
+// Name: Louai Hammad
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-01-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter side length: ";
+	int sideLength;
+	cin >> sideLength;
+	double perimeter = 4 * sideLength;
+	double area = sideLength * sideLength;
+	cout << "Perimeter = " << perimeter << endl;
+	cout << "Area = " << area << endl;
+}
